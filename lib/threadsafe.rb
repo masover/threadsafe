8a60d29285cecd6cc63158c1e91bda17534a3a8e
David Masover <dave@3mix.com>
2009-01-03 16:42:40 -0600

    Added some more classes, and bumped gem version.

--- lib/threadsafe.rb
+++ lib/threadsafe.rb
@@ -3,18 +3,39 @@
 #   (Otherwise, we'd either have to require them here, or intercept every
 #    require, which we can't do until autoload is fixed.)
 
-{
+unsafe_classes = [
   # Default to false
-  Object => false
-}.each_pair do |klass, safe|
+  Object
+]
+
+safe_classes = [
+  # Immutables
+  TrueClass,
+  FalseClass,
+  NilClass,
+  Symbol,
+  Numeric
+]
+
+# TODO: Are frozen strings safe? If so, what's the sanest implementation?
+# Since #freeze and #frozen? seem to exist on Object, it can't be global.
+
+
+# Unrolled to avoid ugly evals or define_method overhead.
+# It's simple enough anyway.
+
+safe_classes.each do |klass|
   klass.class_eval do
     unless method_defined? :threadsafe?
-      # Hack to avoid define_method overhead
-      if safe
-        def threadsafe?; true; end
-      else
-        def threadsafe?; false; end
-      end
+      def threadsafe?; true; end
+    end
+  end
+end
+
+unsafe_classes.each do |klass|
+  klass.class_eval do
+    unless method_defined? :threadsafe?
+      def threadsafe?; false; end
     end
   end
 end
--- threadsafe.gemspec
+++ threadsafe.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name = 'threadsafe'
-  s.version = '0.0.3'
+  s.version = '0.1.0'
   s.date = '2009-01-03'
   s.summary = 'Adds a #threadsafe? method to Ruby classes.'
   s.email = 'dave@3mix.com'
